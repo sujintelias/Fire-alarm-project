ea5d54cc6ee42ccad0cb928e54b47e7f3427ddc0
Sujin.T Elias <Sujintelias789@gmail.com>
2015-05-08 13:07:49 +0530

    sms set

--- Python/udpfireclient.py
+++ Python/udpfireclient.py
@@ -1,17 +1,23 @@
 import socket
 import serial
 server=('127.0.0.1',5000)
+obj=serial.Serial('COM1',9600,serial.EIGHTBITS,serial.PARITY_NONE,serial.STOPBITS_ONE,1)
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 def cmnd(cmd):
-    cmd=(cmd+'/r/n')
-    return cmd
+    cmd=(cmd+'\r\n')
     obj.write(cmd)
-msg=raw_input("->")
+    reply=obj.read(100)
+    print reply
+cmnd('AT')
+cmnd('ATEO')
+cmnd('AT+CMGF=1')
+cmnd('AT+CNMI=1,2,0,0,0,')
+'''msg=raw_input("->") 
 while(1):
     s.sendto(msg,server)
     if msg=='q':
         s.close()
         break
     else:
-        msg=raw_input("->")
+        msg=raw_input("->")'''
     
