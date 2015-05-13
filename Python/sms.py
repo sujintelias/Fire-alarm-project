0c714b3057a8cb455abeb604a1ed716e9007d07f
Sujin.T Elias <Sujintelias789@gmail.com>
2015-05-13 12:41:37 +0530

    msg n sms

--- Python/sms.py
+++ Python/sms.py
@@ -3,21 +3,18 @@ import serial
 server=('127.0.0.1',5000)
 obj=serial.Serial('COM1',9600,serial.EIGHTBITS,serial.PARITY_NONE,serial.STOPBITS_ONE,1)
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-def cmnd(cmd):
-    cmd=(cmd+'\r\n')
-    obj.write(cmd)
-    reply=obj.read(100)
-    print reply
-cmnd('AT')
-cmnd('ATEO')
-cmnd('AT+CMGF=1')
-cmnd('AT+CNMI=1,2,0,0,0')
-msg=raw_input("->") 
-while(1):
+sms=obj.read(100)
+while(sms!=0):
+    if len(sms)>1:
+        sms=sms.split('\n')
+        sms=sms[2]
+        print sms
+        #s.sendto(sms,server)
+'''msg=raw_input("->") 
+while(msg!=0):
     s.sendto(msg,server)
     if msg=='q':
         s.close()
         break
     else:
-        msg=raw_input("->")
-    
+        msg=raw_input("->") '''   
