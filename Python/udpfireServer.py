bc348aea9ef86bfd6d6b58eef71999c15989f27f
Sarath M <sarathm00@gmail.com>
2015-05-28 11:39:39 +0530

    Renamed files

--- Python/udpfireServer.py
+++ Python/udpfireServer.py
@@ -2,7 +2,7 @@ import socket
 import serial
 host=''
 port=5000
-obj=serial.Serial('COM1',9600,serial.EIGHTBITS,serial.PARITY_NONE,serial.STOPBITS_ONE,1) 
+obj=serial.Serial('/dev/ttyUSB0',9600,serial.EIGHTBITS,serial.PARITY_NONE,serial.STOPBITS_ONE,1) 
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 print"server stared"
 s.bind((host,port))
@@ -12,7 +12,7 @@ while(1):
                 obj.write(data)
                 reply=obj.read(100)
                 print"Replay from Serial:"+str(reply)
-                s.close()
+                #s.close()
                 break
         else:
                 print"sending to serial->"+str(data)
