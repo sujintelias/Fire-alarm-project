58384e17fb5d011c50f4ca263b3e2f75395405d8
Sujin.T Elias <Sujintelias789@gmail.com>
2015-05-28 11:26:34 +0530

    final code

--- Python/sensor.py
+++ Python/sensor.py
@@ -1,10 +1,9 @@
 import socket
 import serial
-server=('127.0.0.1',5000)
+server=('192.168.1.16',5000)
 obj=serial.Serial('COM13',9600,serial.EIGHTBITS,serial.PARITY_NONE,serial.STOPBITS_ONE,1)
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 while True:
     sen=obj.read(100)
-    if len(sen)>1:
-        print sen
-        s.sendto(sen,server)
+    print sen
+    s.sendto(sen,server)
