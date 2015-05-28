000ef9de351172f9b599476daf8c1b679862203d
Sujin.T Elias <Sujintelias789@gmail.com>
2015-05-28 11:30:15 +0530

    Deleted junk files

--- Python/my test .py
+++
@@ -1,9 +0,0 @@
-import serial
-obj=serial.Serial('COM1',9600,serial.EIGHTBITS,serial.PARITY_NONE,serial.STOPBITS_ONE,1)
-def cmnd(cmd):
-    cmd=(cmd+'\r\n')
-    return cmd
-obj.write(cmnd('AT'))
-reply=obj.read(100)
-print reply
-
