312d402180b17abf5b6d537763162ff36a745a22
Sarath M <sarathm00@gmail.com>
2015-05-29 18:49:50 +0530

    Test code

--- Python/udpfireServer.py
+++ Python/udpfireServer.py
@@ -1,21 +1,20 @@
 import socket
 import serial
+import time
 host=''
 port=5000
-obj=serial.Serial('/dev/ttyUSB0',9600,serial.EIGHTBITS,serial.PARITY_NONE,serial.STOPBITS_ONE,1) 
+obj=serial.Serial('/dev/ttyS0',9600,serial.EIGHTBITS,serial.PARITY_NONE,serial.STOPBITS_ONE,1) 
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 print"server stared"
 s.bind((host,port))
 while(1):
-        data,addr=s.recvfrom(1024)
-        if data=='q':
-                obj.write(data)
+        #data,addr=s.recvfrom(1024)
+        obj.write('f')
+        #print"Sending to serial->"+str(data)
+        print"Sending to serial->"+str('f')
+        while True:
                 reply=obj.read(100)
-                print"Replay from Serial:"+str(reply)
-                #s.close()
-                break
-        else:
-                print"sending to serial->"+str(data)
-                obj.write(data)
-                reply=obj.read(100)
-                print"Replay from Serial:"+str(reply)
+                if len(reply)>0:
+                        print"Replay from Serial:"+str(reply)
+                        break
+        time.sleep(1)
