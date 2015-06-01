220e6c4b8468b35eb6c48a39c109060fd4fdbb19
Sarath M <sarathm00@gmail.com>
2015-06-01 13:10:33 +0530

    Server side python script

--- Python/smsServer.py
+++ Python/smsServer.py
@@ -1,13 +1,17 @@
 import socket
 import serial
-server=('127.0.0.1',5000)
-obj=serial.Serial('/dev/ttyS0',9600,serial.EIGHTBITS,serial.PARITY_NONE,serial.STOPBITS_ONE,1)
-s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-while True:
-    sms=obj.read(100)
-    if len(sms)>1:
-        sms=sms.split('\n')
-        sms=sms[2]
-        print sms
-        s.sendto(sms,server)
-            
+
+def main():
+	while True:
+	    sms=obj.read(100)
+	    if len(sms)>1:
+	        sms=sms.split('\n')
+	        sms=sms[2]
+	        print sms
+	        s.sendto(sms,server)
+
+if __name__ == '__main__':
+	server=('127.0.0.1',5000)
+	obj=serial.Serial('/dev/ttyUSB0',9600,serial.EIGHTBITS,serial.PARITY_NONE,serial.STOPBITS_ONE,1)
+	s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+	main()            
--- Python/udpfireServer.py
+++ Python/udpfireServer.py
@@ -1,20 +1,25 @@
 import socket
 import serial
 import time
-host=''
-port=5000
-obj=serial.Serial('/dev/ttyS0',9600,serial.EIGHTBITS,serial.PARITY_NONE,serial.STOPBITS_ONE,1) 
-s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-print"server stared"
-s.bind((host,port))
-while(1):
-        #data,addr=s.recvfrom(1024)
-        obj.write('f')
-        #print"Sending to serial->"+str(data)
-        print"Sending to serial->"+str('f')
-        while True:
-                reply=obj.read(100)
-                if len(reply)>0:
-                        print"Replay from Serial:"+str(reply)
-                        break
-        time.sleep(1)
+
+def main():
+        while(1):
+                data,addr=s.recvfrom(1024)
+                obj.write(data)
+                print"Sending to serial->"+str(data)
+                #print"Sending to serial->"+str('f')
+                while True:
+                        reply=obj.read(100)
+                        if len(reply)>0:
+                                print"Replay from Serial:"+str(reply)
+                                break
+                time.sleep(1)
+
+if __name__ == '__main__':
+        host=''
+        port=5000
+        obj=serial.Serial('/dev/ttyS0',9600,serial.EIGHTBITS,serial.PARITY_NONE,serial.STOPBITS_ONE,1) 
+        s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        print"server stared"
+        s.bind((host,port))
+        main()
